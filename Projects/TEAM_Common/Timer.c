bc18572bc01d0d97753edbdd53168736230ccd58
Delvira <hexeluna@hotmail.com>
2017-03-21 11:15:37 +0100

    timer

--- Projects/TEAM_Common/Timer.c
+++ Projects/TEAM_Common/Timer.c
@@ -20,10 +20,19 @@
 #endif
 #include "TMOUT1.h"
 
+
 void TMR_OnInterrupt(void) {
   /* this one gets called from an interrupt!!!! */
   /*! \todo Add code for a blinking LED here */
-	EVNT_SetEvent(EVNT_LED_HEARTBEAT);
+	static unsigned int cntr = 0;
+	*define BLINK_PERIOD_MS 1000
+
+	cntr++;
+#if PL_CONFIG_HAS_EVENTS
+	if ((cntr%(BLINK_PERIOD_MS/TMR_TICK_MS))==0) {
+		EVNT_SetEvent(EVNT_LED_HEARTBEAT);
+	}
+#endif
 }
 
 void TMR_Init(void) {
