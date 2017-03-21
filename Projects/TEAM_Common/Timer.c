14cc89f87a0806e90928e8a4bfcb7a2855a8707d
Delvira <hexeluna@hotmail.com>
2017-03-21 11:34:18 +0100

    timer.c

--- Projects/TEAM_Common/Timer.c
+++ Projects/TEAM_Common/Timer.c
@@ -24,11 +24,11 @@
 void TMR_OnInterrupt(void) {
   /* this one gets called from an interrupt!!!! */
   /*! \todo Add code for a blinking LED here */
-	static unsigned int cntr = 0;
-	#define BLINK_PERIOD_MS 1000
-	cntr++;
+  static unsigned int cntr = 0;
+  #define BLINK_PERIOD_MS 1000
+  cntr++;
 #if PL_CONFIG_HAS_EVENTS
-	if ((cntr%(BLINK_PERIOD_MS/TMR_TICK_MS))==0) {
+	if ((cntr%(BLINK_PERIOD_MS/TMR_TICK_MS))==0) { /* every two seconds */
 		EVNT_SetEvent(EVNT_LED_HEARTBEAT);
 	}
 #endif
