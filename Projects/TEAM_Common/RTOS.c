0accca8d0ca75b245d4a595b591546f5263026ce
tamattma <remo.mattmann@hotmail.com>
2017-04-04 11:41:47 +0200

    blink 2 leds as tasks

--- Projects/TEAM_Common/RTOS.c
+++ Projects/TEAM_Common/RTOS.c
@@ -8,11 +8,44 @@
 #if PL_CONFIG_HAS_RTOS
 #include "RTOS.h"
 #include "FRTOS1.h"
+#include "LED.h"
+
+static void BlinkyTask(void *pvParameters){
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	for(;;){
+		LED1_Neg();
+		vTaskDelayUntil(&xLastWakeTime, 500/portTICK_PERIOD_MS);
+	}
+}
+
+static void BlinkyTask2(void *pvParameters){
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	for(;;){
+		LED2_Neg();
+		vTaskDelayUntil(&xLastWakeTime, 300/portTICK_PERIOD_MS);
+	}
+}
 
 void RTOS_Init(void) {
   /*! \todo Create tasks here */
 #if PL_LOCAL_CONFIG_BOARD_IS_ROBO
 
+	xTaskHandle taskBlinkHndl ;
+	BaseType_t res = xTaskCreate( 	BlinkyTask,
+									"Blinky",
+									configMINIMAL_STACK_SIZE+50,
+									(void*) NULL,
+									tskIDLE_PRIORITY,
+									&taskBlinkHndl
+									);
+	xTaskHandle taskBlinkHndl2 ;
+		BaseType_t res2 = xTaskCreate( 	BlinkyTask2,
+										"Blinky2",
+										configMINIMAL_STACK_SIZE+50,
+										(void*) NULL,
+										tskIDLE_PRIORITY,
+										&taskBlinkHndl2
+										);
 #endif
 
 #if PL_LOCAL_CONFIG_BOARD_IS_REMOTE
